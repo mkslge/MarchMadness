1e8f98701bbc2acc28b23befa79cf3bf93d0fe3c
Mark Seeliger <117680637+mkslge@users.noreply.github.com>
2025-04-10 19:26:55 -0500

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,10 +1,20 @@
 package org.example;
 
-public class Team {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class Team {
+    int year;
+
+    @JsonProperty("Seed")
     int seed;
+
+    @JsonProperty("Team")
     String name;
+
+    @JsonProperty("AdjT")
     double adjT;
+
+    @JsonProperty("AdjEM")
     double adjEM;
 
 
@@ -19,14 +29,26 @@ public class Team {
         this.adjT = adjT;
     }
 
+    public Team(int year, String name, int seed) {
+        this.year = year;
+        this.name = new String(name);
+        this.seed = seed;
+
+    }
+
     public Team() {
         this.name = "";
+    }
+    public Team(int year) {
+        this.name = "";
+        this.year = year;
     }
 
     public Team(Team other) {
         if (other == null) {
             throw new IllegalArgumentException("The provided team cannot be null");
         }
+        this.year = other.year;
         this.name =  other.name;
         this.seed = other.seed;
         this.adjEM = other.adjEM;
@@ -43,6 +65,24 @@ public class Team {
         this.seed = other.seed;
         this.adjEM = other.adjEM;
         this.adjT = other.adjT;
+    }
+
+
+    int getYear() {
+        return this.year;
+    }
+
+    String getName() {
+        return new String(this.name);
+    }
+
+
+    public int getSeed() {
+        return this.seed;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
 }
